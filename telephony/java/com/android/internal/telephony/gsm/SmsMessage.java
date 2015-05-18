10c05de98a249a23afb11ac66f2d235dba47d7c0
Anthony Cox <motorhead1991@hotmail.com>
2015-05-18 01:22:27 -0600

    cleanup #1 of the Telephony merge

--- telephony/java/com/android/internal/telephony/gsm/SmsMessage.java
+++ telephony/java/com/android/internal/telephony/gsm/SmsMessage.java
@@ -24,6 +24,7 @@ import android.util.Log;
 import com.android.internal.telephony.IccUtils;
 import com.android.internal.telephony.EncodeException;
 import com.android.internal.telephony.GsmAlphabet;
+import com.android.internal.telephony.SimRegionCache;
 import com.android.internal.telephony.SmsHeader;
 import com.android.internal.telephony.SmsMessageBase;
 
